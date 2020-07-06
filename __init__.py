cdbd638a7ea1a5311f6fc6b078ea27e9d208e308
Paul Grimes <pgrimes@cfa.harvard.edu>
2020-07-06 18:56:17 -0400

    Update __init__.py
    Add address and channel back in

--- __init__.py
+++ __init__.py
@@ -21,8 +21,10 @@ class MCP4728Actor(ActorBase):
                               description="0ms will disable notifications completely")
 
     def init(self):
+        address = int(self.address)
+        channel = int(self.channel)
         #self.dac = mcp4728.MCP4728(self.address)
-        #cbpi.notify("Connected to MCP4728", "DAC Address {:d}".format(self.address),
+        cbpi.notify("Connected to MCP4728", "DAC Address {:d}".format(self.address),
         #            "DAC Channel {:d}".format(self.channel), timeout=None, type="danger")
         # if self.voltage_ref == "Vdd":
         #     self.dac.set_vref(channel, 0)
