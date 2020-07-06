da0c5ca04a5d7bb4157b58d9ca130ac45b34f3df
Paul Grimes <pgrimes@cfa.harvard.edu>
2020-07-06 18:18:06 -0400

    Simplify code to try and get something to work.

--- __init__.py
+++ __init__.py
@@ -11,29 +11,26 @@ class MCP4728Actor(ActorBase):
 
     The on/off state of the actor can be controlled either by setting the DAC output to zero, or using an additional actor.
     """
-    address = Property.Select("DAC Address", [0,1,2,3,4,5,6,7], description="Minor address of the MCP4728 DAC unit. Use 0 unless you have changed the address manually")
-    channel = Property.Select("Channel", [0,1,2,3], description="The channel to output the voltage")
-    voltage_ref = Property.Select("Reference Voltage", ["Vdd", "Internal 2.048V", "Internal 4.096V"], description="The voltage reference to use.")
-    power_control = Property.Select("Power On/Off Mode", ["DAC", "Actor"], description="Power off by setting DAC output to zero, or using another actor")
-    power_actor = Property.Actor(label="Power Actor", description="Select the actor to use for on/off control")
-    timeout = Property.Number("Notification duration (ms)", True, 5000, description="0ms will disable notifications completely")
+    address = Property.Select("DAC Address", [0,1,2,3,4,5,6,7],
+                              description="Minor address of the MCP4728 DAC unit. Use 0 unless you have changed the address manually")
+    channel = Property.Select("Channel", [0,1,2,3],
+                              description="The channel to output the voltage")
+    timeout = Property.Number("Notification duration (ms)", True, 5000,
+                              description="0ms will disable notifications completely")
 
     def init(self):
-        address = int(self.address)
-        channel = int(self.channel)
-
-        self.dac = mcp4728.MCP4728(address)
+        self.dac = mcp4728.MCP4728(self.address)
         cbpi.notify("Connected to MCP4728", "DAC Address {:d}".format(self.address), "DAC Channel {:d}".format(self.channel), timeout=None, type="danger")
-        if self.voltage_ref == "Vdd":
-            self.dac.set_vref(channel, 0)
-        else:
-            self.dac.set_vref(channel, 1)
-            if self.voltage_ref == "Internal 4.096V":
-                self.dac.set_gain(channel, 1)
-            else:
-                self.dac.set_gain(channel, 0)
+        # if self.voltage_ref == "Vdd":
+        #     self.dac.set_vref(channel, 0)
+        # else:
+        #     self.dac.set_vref(channel, 1)
+        #     if self.voltage_ref == "Internal 4.096V":
+        #         self.dac.set_gain(channel, 1)
+        #     else:
+        #         self.dac.set_gain(channel, 0)
 
-        self.value = self.dac.get_value(channel)
+        self.value = self.dac.get_value(self.channel)
         cbpi.notify("MCP4728 Channel {:d} Value {:d}".format(self.channel, self.value), timeout=None, type="danger")
 
     def set_power(self, power):
@@ -43,16 +40,16 @@ class MCP4728Actor(ActorBase):
 
         channel = int(self.channel)
 
-        power_actor_name = ""
-        for idx, value in cbpi.cache["actors"].iteritems():
-            if idx == int(self.power_actor):
-                power_actor_name = value.name
-        for idx, value in cbpi.cache["actors"].iteritems():
-            if idx == int(self.power_actor):
-                if value.state == 0:
-                    pass
-                elif value.state == 1:
-                    self.dac.write_value(channel, self.value)
+        # power_actor_name = ""
+        # for idx, value in cbpi.cache["actors"].iteritems():
+        #     if idx == int(self.power_actor):
+        #         power_actor_name = value.name
+        # for idx, value in cbpi.cache["actors"].iteritems():
+        #     if idx == int(self.power_actor):
+        #         if value.state == 0:
+        #             pass
+        #         elif value.state == 1:
+        self.dac.write_value(channel, self.value)
 
     def off(self):
         """Switch the actor off"""
