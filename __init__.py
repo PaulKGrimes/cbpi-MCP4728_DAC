d66a2d4c5523c494f6162a0b58e03a649956c6c8
Paul Grimes <pgrimes@cfa.harvard.edu>
2020-07-08 06:30:23 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -49,7 +49,9 @@ class MCP4728Actor(ActorBase):
         # CBPI Actor sets UI state to 0 and power to 100 in post_init_callback, called after this method,
         # so set power here to 100 and state to off to make sure we start in a consistent state.
         self.api.switch_actor_off(self.id)
+        self.state = 0
         self.api.actor_power(self.id, 100)
+        self.value = 4095
 
     def set_power(self, power):
         """Set the power as a percentage of the range between minimum and maximum power"""
